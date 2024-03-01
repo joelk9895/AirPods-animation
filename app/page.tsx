62d8d79b0e4951230b8d8646b5dfcb191c628789
Joel K George <joelk9895@gmail.com>
2024-03-02 00:11:08 +0530

    final

--- app/page.tsx
+++ app/page.tsx
@@ -45,7 +45,6 @@ export default function Home() {
 
     preloadImages();
   }, [queryKey]);
-
   useEffect(() => {
     if (!imagesLoaded) return;
 
@@ -76,7 +75,7 @@ export default function Home() {
   const reloadImages = () => {
     setQueryKey(Date.now());
   };
-
+  if (!imagesLoaded) return <div>Loading...</div>;
   return (
     <main
       className="flex h-[500vh] min-h-screen flex-col items-center justify-between bg-black"
